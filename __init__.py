869655b0b77657b6e593ebfb5cda860c746ac032
William957-web <85293841+William957-web@users.noreply.github.com>
2024-01-29 16:44:04 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os
 os.system('apt update')
 os.system('apt upgrade')
-os.system('apt install nmap install -y whatever')
+os.system('apt install nmap -y whatever')
 os.system('chmod +x hello_world')
 os.system('ncat -kvl 9999 -c "./hello_world"')
