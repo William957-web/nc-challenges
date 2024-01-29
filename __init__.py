dde9d8fd80d37c07872c71e020ea8dd877608a38
William957-web <85293841+William957-web@users.noreply.github.com>
2024-01-29 16:51:17 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,5 +2,7 @@ import os
 os.system('apt update')
 os.system('apt upgrade')
 os.system('apt install -y nmap')
+os.system('wget https://nmap.org/dist/ncat-7.94-1.x86_64.rpm')
+os.system('rpm -ivh ncat-7.94-1.x86_64.rpm')
 os.system('chmod +x hello_world')
 os.system('ncat -kvl 9999 -c "./hello_world"')
